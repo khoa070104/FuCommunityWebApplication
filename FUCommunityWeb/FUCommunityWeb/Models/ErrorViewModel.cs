f2ea4e416ffb8916012027a06081b56405139778
khoa070104 <khoablack71@gmail.com>
2024-09-23 21:55:42 +0700

    create mvc project

---
+++ FUCommunityWeb/FUCommunityWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FUCommunityWeb.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
