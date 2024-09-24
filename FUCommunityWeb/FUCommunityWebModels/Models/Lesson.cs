75e6b2f49adef5e3feac6abea06361d861eb2d84
khoa070104 <khoablack71@gmail.com>
2024-09-24 19:48:58 +0700

    config_code_first

---
+++ FUCommunityWeb/FUCommunityWebModels/Models/Lesson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FuCommunityWebModels.Models
+{
+	public class Lesson
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int LessonID { get; set; }
+
+		public int? CourseID { get; set; }
+
+		[Required]
+		[MaxLength(255)]
+		public string Title { get; set; }
+
+		public string Content { get; set; }
+
+		[DataType(DataType.DateTime)]
+		public DateTime? CreatedDate { get; set; } = DateTime.Now;
+
+		[DataType(DataType.DateTime)]
+		public DateTime? UpdatedDate { get; set; }
+
+		[MaxLength(255)]
+		public string LessonUrl { get; set; }
+
+		[MaxLength(50)]
+		public string Status { get; set; }
+
+		// Navigation property
+		[ForeignKey("CourseID")]
+		public Course Course { get; set; }
+	}
+}
---
+++ FUCommunityWeb/FUCommunityWebModels/Models/Post.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+namespace FuCommunityWebModels.Models
+{
+	public class Post
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int PostID { get; set; }
+
+		public int? UserID { get; set; }
+		public int? CategoryID { get; set; }
+
+		[Required]
+		[MaxLength(255)]
+		public string Title { get; set; }
+
+		public string Content { get; set; }
+
+		[MaxLength(255)]
+		public string PostImage { get; set; }
+
+		[DataType(DataType.DateTime)]
+		public DateTime? CreatedDate { get; set; } = DateTime.Now;
+
+		[DataType(DataType.DateTime)]
+		public DateTime? UpdatedDate { get; set; }
+
+		[MaxLength(50)]
+		public string Status { get; set; }
+
+		// Navigation properties
+		[ForeignKey("CategoryID")]
+		public Category Category { get; set; }
+
+		[ForeignKey("UserID")]
+		public User User { get; set; }
+
+		public ICollection<Comment> Comments { get; set; }  // Một Post có nhiều Comments
+		public ICollection<Document> Documents { get; set; }  // Một Post có nhiều Documents
+		public ICollection<Vote> Votes { get; set; }  // Một Post có nhiều Votes
+		public ICollection<Point> Points { get; set; }
+	}
+}
