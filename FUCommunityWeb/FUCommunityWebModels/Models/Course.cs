c08f3b9e02d1aa303f2750eafde26e5067d0db31
hoang <=>
2024-10-14 13:48:11 +0700

    "Fix CRUD course and filter course"

--- FUCommunityWeb/FUCommunityWebModels/Models/Course.cs
+++ FUCommunityWeb/FUCommunityWebModels/Models/Course.cs
@@ -6,8 +6,8 @@ using System.Reflection.Metadata;
 
 namespace FuCommunityWebModels.Models
 {
-	public class Course
-	{
+    public class Course
+    {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CourseID { get; set; }
@@ -31,17 +31,28 @@ namespace FuCommunityWebModels.Models
 
         [StringLength(50)]
         public string Status { get; set; }
-        public int Semester { get; set; } 
+        public int Semester { get; set; }
 
         public int CategoryID { get; set; }
-        [ForeignKey("CategoryID")] 
+        [ForeignKey("CategoryID")]
         public Category Category { get; set; }
 
-        [ForeignKey("UserID")] 
+        [ForeignKey("UserID")]
         public ApplicationUser User { get; set; }
 
         public virtual ICollection<Lesson> Lessons { get; set; }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
         public virtual ICollection<Document> Documents { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+        [NotMapped]
+        public double Rate
+        {
+            get
+            {
+                if (Reviews == null || !Reviews.Any())
+                    return 0;
+                return Reviews.Average(r => r.Rating);
+            }
+        }
     }
 }
