736734751f541b5bd9b324c9c5154689609a0455
hoang <=>
2024-10-22 12:40:38 +0700

    "update userVM"

--- FUCommunityWeb/FUCommunityWebModels/ViewModels/UserVM.cs
+++ FUCommunityWeb/FUCommunityWebModels/ViewModels/UserVM.cs
@@ -11,19 +11,29 @@ namespace FuCommunityWebModels.ViewModels
 {
     public class UserVM
     {
-		public ApplicationUser User { get; set; }
-		public List<Enrollment> Enrollments { get; set; }
-		public List<Post> Post { get; set; }
-		//public string FullName { get; set; }
-		//public string Bio { get; set; }
-		//public string Address { get; set; }
-		//public string Gender { get; set; }
-		//[ValidateNever]
-		//public DateTime DOB { get; set; }
-		//public string PhoneNumber { get; set; }
-		//[ValidateNever]
-		//public string Description { get; set; }
-		//[ValidateNever]
-		//public string AvatarImage { get; set; }
-	}
+        public ApplicationUser User { get; set; }
+        public List<Enrollment> Enrollments { get; set; }
+        public List<Post> Post { get; set; }
+        //public string FullName { get; set; }
+        //public string Bio { get; set; }
+        //public string Address { get; set; }
+        //public string Gender { get; set; }
+        //[ValidateNever]
+        //public DateTime DOB { get; set; }
+        //public string PhoneNumber { get; set; }
+        //[ValidateNever]
+        //public string Description { get; set; }
+        //[ValidateNever]
+        //public string AvatarImage { get; set; }
+        public bool IsCurrentUser { get; set; }
+        public bool IsFollowing { get; set; }
+
+        // Thêm thuộc tính để lưu trữ danh sách người theo dõi
+        public List<ApplicationUser> Followers { get; set; }
+        public bool IsMentor { get; set; }
+
+        // Thêm các thuộc tính mới để lưu trữ tổng số Posts và Questions
+        public int TotalPosts { get; set; }
+        public int TotalQuestions { get; set; }
+    }
 }
