81a50ebeef1f928a43483345d43ad6866c1febc5
hoang <=>
2024-10-22 21:38:06 +0700

    "update VM"

--- FUCommunityWeb/FUCommunityWebModels/ViewModels/EditPostVM.cs
+++ FUCommunityWeb/FUCommunityWebModels/ViewModels/EditPostVM.cs
@@ -1,31 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FuCommunityWebModels.Models;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FuCommunityWebModels.Models;
 
 namespace FuCommunityWebModels.ViewModels
 {
-    public class CreatePostVM
+    public class EditPostVM
     {
+        public int PostID { get; set; }
+
         [Required]
         public string Title { get; set; }
+
         [Required]
         public string Content { get; set; }
 
         public int? CategoryID { get; set; }
-        public string UserID { get; set; }
-        public int PostID { get; set; }
         public string Tag { get; set; }
         public int Type { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public DateTime? UpdatedDate { get; set; }
-        public string PostImage { get; set; }
-        [NotMapped]
+
         [Display(Name = "Post Image")]
         public IFormFile PostImageFile { get; set; }
         public List<Category> Categories { get; set; }
--- FUCommunityWeb/FUCommunityWebModels/ViewModels/ForumVM.cs
+++ FUCommunityWeb/FUCommunityWebModels/ViewModels/ForumVM.cs
@@ -10,6 +10,8 @@ namespace FuCommunityWebModels.ViewModels
     public class ForumVM
     {
         public List<Post> Posts { get; set; }
-        public List<Category> Categories { get; set; }  
+        public List<Category> Categories { get; set; }  // Danh sách các danh mục
+        public CreateCategoryVM CreateCategory { get; set; }  // ViewModel cho tạo danh mục
+        public EditCategoryVM EditCategory { get; set; }  // ViewModel cho chỉnh sửa danh mục
     }
 }
